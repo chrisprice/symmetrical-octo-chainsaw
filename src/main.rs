5ce4b98bda0a3dc779b528d0b6d029925d2f12f4
Chris Price <price.c@gmail.com>
2025-09-14 14:27:47 +0100

    Implement HTTP task and WebSocket handler

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use crate::{
 };
 use cyw43_pio::PioSpi;
 use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+use edge_nal_embassy::TcpBuffers;
 use embassy_executor::Spawner;
 use embassy_net::{Stack, StackResources};
 use embassy_rp::i2c::{self};
@@ -26,6 +28,15 @@ use embassy_rp::{bind_interrupts, clocks::RoscRng};
 use embassy_time::Duration;
 use embassy_time::Timer;
 use static_cell::StaticCell;
+
+use edge_http::Method;
+use edge_http::io::Error;
+use edge_http::io::server::{Connection, DefaultServer, Handler};
+use edge_http::ws::MAX_BASE64_KEY_RESPONSE_LEN;
+use edge_nal::TcpBind;
+use edge_ws::{FrameHeader, FrameType};
+
+use embedded_io_async::{Read, Write};
 
 bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => adc::InterruptHandler;
@@ -46,7 +57,7 @@ async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'sta
 }
 
 #[embassy_executor::task]
-async fn conn_led_task(mut led: Output<'static>, stack: &'static Stack<'static>) -> ! {
+async fn conn_led_task(mut led: Output<'static>, stack: Stack<'static>) -> ! {
     loop {
         // Slow blink until config up
         while !stack.is_config_up() {
@@ -73,6 +84,132 @@ async fn conn_led_task(mut led: Output<'static>, stack: &'static Stack<'static>)
     }
 }
 
+#[embassy_executor::task]
+async fn http_task(stack: Stack<'static>) -> ! {
+    let addr = "0.0.0.0:80";
+
+    static BUFFERS: StaticCell<TcpBuffers<1, 512, 512>> = StaticCell::new();
+    let buffers = BUFFERS.init(TcpBuffers::new());
+
+    let tcp = edge_nal_embassy::Tcp::new(stack, buffers);
+    let acceptor = tcp
+        .bind(addr.parse().unwrap())
+        .await
+        .unwrap();
+
+    let mut server = DefaultServer::new();
+    server.run(None, acceptor, WsHandler).await.unwrap();
+    defmt::unreachable!()
+}
+
+
+#[derive(Debug)]
+enum WsHandlerError<C, W> {
+    Connection(C),
+    Ws(W),
+}
+
+impl<C, W> From<C> for WsHandlerError<C, W> {
+    fn from(e: C) -> Self {
+        Self::Connection(e)
+    }
+}
+
+struct WsHandler;
+
+impl Handler for WsHandler {
+    type Error<E>
+        = WsHandlerError<Error<E>, edge_ws::Error<E>>
+    where
+        E: core::fmt::Debug;
+
+    async fn handle<T, const N: usize>(
+        &self,
+        _task_id: impl core::fmt::Display + Clone,
+        conn: &mut Connection<'_, T, N>,
+    ) -> Result<(), Self::Error<T::Error>>
+    where
+        T: Read + Write,
+    {
+        let headers = conn.headers()?;
+
+        if headers.method != Method::Get {
+            conn.initiate_response(405, Some("Method Not Allowed"), &[])
+                .await?;
+        } else if headers.path != "/" {
+            conn.initiate_response(404, Some("Not Found"), &[]).await?;
+        } else if !conn.is_ws_upgrade_request()? {
+            conn.initiate_response(200, Some("OK"), &[("Content-Type", "text/plain")])
+                .await?;
+
+            conn.write_all(b"Initiate WS Upgrade request to switch this connection to WS")
+                .await?;
+        } else {
+            let mut buf = [0_u8; MAX_BASE64_KEY_RESPONSE_LEN];
+            conn.initiate_ws_upgrade_response(&mut buf).await?;
+
+            conn.complete().await?;
+
+            info!("Connection upgraded to WS, starting a simple WS echo server now");
+
+            // Now we have the TCP socket in a state where it can be operated as a WS connection
+            // Run a simple WS echo server here
+
+            let mut socket = conn.unbind()?;
+
+            let mut buf = [0_u8; 8192];
+
+            loop {
+                let mut header = FrameHeader::recv(&mut socket)
+                    .await
+                    .map_err(WsHandlerError::Ws)?;
+                let payload = header
+                    .recv_payload(&mut socket, &mut buf)
+                    .await
+                    .map_err(WsHandlerError::Ws)?;
+
+                match header.frame_type {
+                    FrameType::Text(_) => {
+                        info!(
+                            "Got {}, with payload \"{}\"",
+                            header,
+                            core::str::from_utf8(payload).unwrap()
+                        );
+                    }
+                    FrameType::Binary(_) => {
+                        info!("Got {}, with payload {:?}", header, payload);
+                    }
+                    FrameType::Close => {
+                        info!("Got {}, client closed the connection cleanly", header);
+                        break;
+                    }
+                    _ => {
+                        info!("Got {}", header);
+                    }
+                }
+
+                // Echo it back now
+
+                header.mask_key = None; // Servers never mask the payload
+
+                if matches!(header.frame_type, FrameType::Ping) {
+                    header.frame_type = FrameType::Pong;
+                }
+
+                info!("Echoing back as {}", header);
+
+                header.send(&mut socket).await.map_err(WsHandlerError::Ws)?;
+                header
+                    .send_payload(&mut socket, payload)
+                    .await
+                    .map_err(WsHandlerError::Ws)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
@@ -92,7 +229,7 @@ async fn main(spawner: Spawner) {
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, board.pwr, board.spi, fw).await;
-    spawner.spawn(cyw43_task(runner));
+    unwrap!(spawner.spawn(cyw43_task(runner)));
 
     control.init(clm).await;
     control
@@ -112,11 +249,8 @@ async fn main(spawner: Spawner) {
             rng.next_u64()
         });
 
-    static STACK: StaticCell<Stack<'static>> = StaticCell::new();
-    let stack = STACK.init(stack);
-
-    spawner.spawn(net_task(runner));
-    spawner.spawn(conn_led_task(board.user_led_2, stack));
+    unwrap!(spawner.spawn(net_task(runner)));
+    unwrap!(spawner.spawn(conn_led_task(board.user_led_2, stack)));
 
     control
         .start_ap_wpa2(
@@ -125,6 +259,8 @@ async fn main(spawner: Spawner) {
             5,
         )
         .await;
+
+    unwrap!(spawner.spawn(http_task(stack)));
 
     let mut led = board.user_led_1;
     let button = board.user_switch_a;
